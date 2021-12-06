23c98446420b5b11c1a2cdd6717ffbb4f783b086
Cyril <kirill.lihotin@gmail.com>
2021-12-06 16:14:21 +0500

    rest was added

--- src/main/kotlin/com/cyrillihotin/grammartrainer/controller/SentenceController.kt
+++ src/main/kotlin/com/cyrillihotin/grammartrainer/controller/SentenceController.kt
@@ -1,32 +1,19 @@
 package com.cyrillihotin.grammartrainer.controller
 
+import com.cyrillihotin.grammartrainer.model.Sentence
 import com.cyrillihotin.grammartrainer.service.SentenceServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
-
-import org.springframework.stereotype.Controller
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.ui.Model
-import kotlin.reflect.full.declaredMemberProperties
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class SentenceController(
     @Autowired private val service: SentenceServiceImpl
 ) {
-
-    private fun index(id: Int, field: String): Any? {
-        val dbField = service.findById(id)
-        if(dbField != null) {
-            return requireNotNull(dbField::class.declaredMemberProperties
-                .find { it.name == field }).call(dbField)
-        }
-        throw IllegalArgumentException("Record 'english' by id $id not found")
-    }
-
-    @GetMapping("/{id}")
-    fun template(@PathVariable id: Int, model: Model): String {
-        model.addAttribute("russian", index(id, "russian"))
-        model.addAttribute("english", index(id, "english"))
-        return "main"
+    @GetMapping("{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    private fun index(@PathVariable id: Int): Sentence? {
+        return service.findById(id)
     }
 }
